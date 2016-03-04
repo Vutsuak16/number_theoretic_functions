9974e77b1abc08b3326f0742aa2f1ec539475ca9
kaustuv deolal <vutsuak96@gmail.com>
2016-03-04 16:37:14 +0530

    number theoretic functions added

---
+++ tau.py
@@ -0,0 +1,35 @@
+__name__ = "vutsuak"
+
+from scipy.linalg import circulant
+from numpy.linalg import det
+import numpy as np
+
+
+# tau is the number theoretic function that counts the number of divisors of a given integer
+
+def tau(n):
+    ct = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            ct += 1
+    return ct
+
+
+def matrix():
+    determinant = []
+    circulant_matrix = []
+    for i in range(2, 21):
+        array = []
+        for j in range(1, i + 1):
+            l = tau(j)
+            array.append(l)
+
+        circulant_matrix.append(circulant(array))
+        determinant.append(det(np.transpose(circulant_matrix[-1])))
+    for i in range(len(determinant)):
+        print(np.transpose(circulant_matrix[i]))
+        print("the determinant of circulant matrix  for n= " + str(i + 2) + " is " + str(determinant[i]))
+
+
+if __name__ == "vutsuak":
+    matrix()
